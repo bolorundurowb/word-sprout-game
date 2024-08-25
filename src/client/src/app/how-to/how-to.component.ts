6866782172604d346d0a99cb3a10a08360959f37
Bolorunduro Winner-Timothy <bolorundurowb@gmail.com>
2024-08-25 23:13:05 +0100

    ft: add in action buttons

--- src/client/src/app/how-to/how-to.component.ts
+++ src/client/src/app/how-to/how-to.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { TuiTitle } from '@taiga-ui/core';
+import { TuiButton, TuiTitle } from '@taiga-ui/core';
 
 @Component({
   selector: 'ws-how-to',
   standalone: true,
   imports: [
-    TuiTitle
+    TuiTitle,
+    TuiButton
   ],
   templateUrl: 'how-to.component.html',
   styleUrl: 'how-to.component.scss',
