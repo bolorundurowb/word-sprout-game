f83425e10ff0d7b31e2030290e70c9a26a061e36
Bolorunduro Winner-Timothy <bolorundurowb@gmail.com>
2024-08-25 22:38:59 +0100

    ft: add in instructions

--- src/client/src/app/how-to/how-to.component.ts
+++ src/client/src/app/how-to/how-to.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { TuiTitle } from '@taiga-ui/core';
 
 @Component({
   selector: 'ws-how-to',
   standalone: true,
-  imports: [],
+  imports: [
+    TuiTitle
+  ],
   templateUrl: 'how-to.component.html',
   styleUrl: 'how-to.component.scss',
 })
-export class HowToComponent {}
+export class HowToComponent {
+}
