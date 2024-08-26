52048877b6671192c4f631d0c6c4423e1866771f
Bolorunduro Winner-Timothy <bolorundurowb@gmail.com>
2024-08-27 00:22:38 +0100

    ft: add basic new form pages

--- src/client/src/app/new-game/new-game.component.ts
+++ src/client/src/app/new-game/new-game.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { TuiTitle } from '@taiga-ui/core';
 
 @Component({
   selector: 'ws-new-game',
   standalone: true,
-  imports: [],
-  templateUrl: 'new-game.component.html'
+  imports: [
+    TuiTitle
+  ],
+  templateUrl: 'new-game.component.html',
+  styleUrl: 'new-game.component.scss'
 })
-export class NewGameComponent {}
+export class NewGameComponent {
+}
