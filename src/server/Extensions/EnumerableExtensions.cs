8a83f0a6a514141f987dfbe927f36eff3170556f
Bolorunduro Winner-Timothy <bolorundurowb@gmail.com>
2024-09-15 20:08:03 +0100

    ft: add enumerable extensions

---
+++ src/server/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace WordSproutApi.Extensions;
+
+public static class EnumerableExtensions
+{
+    public static int IndexOf<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+    {
+        if (source == null || predicate == null)
+            throw new ArgumentNullException(source == null ? nameof(source) : nameof(predicate));
+
+        var index = 0;
+        foreach (var item in source)
+        {
+            if (predicate(item))
+                return index;
+
+            index++;
+        }
+
+        return -1;
+    }
+}
