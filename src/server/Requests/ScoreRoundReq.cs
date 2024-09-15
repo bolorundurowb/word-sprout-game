5d35aa1bd7cf41edbf5eece1130d2da914a2eefc
Bolorunduro Winner-Timothy <bolorundurowb@gmail.com>
2024-09-15 20:06:48 +0100

    ft: add the score round request model and validator

---
+++ src/server/Requests/ScoreRoundReq.cs
@@ -0,0 +1,10 @@
+﻿namespace WordSproutApi.Requests;
+
+public class ScoreRoundReq
+{
+    public string Username { get; set; }
+
+    public char Character { get; set; }
+
+    public Dictionary<string, int> PlayerScores { get; set; }
+}
---
+++ src/server/Validators/ScoreRoundReqValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using WordSproutApi.Requests;
+
+namespace WordSproutApi.Validators;
+
+public class ScoreRoundReqValidator : AbstractValidator<ScoreRoundReq>
+{
+    public ScoreRoundReqValidator()
+    {
+        RuleFor(x => x.Character)
+            .NotEmpty();
+
+        RuleFor(x => x.Username)
+            .NotNull()
+            .NotEmpty();
+
+        RuleFor(x => x.PlayerScores)
+            .NotNull();
+    }
+}
