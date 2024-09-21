abddff41c095bb00325252b62764b6857ccb51cb
Winner-Timothy Bolorunduro <bolorundurowb@gmail.com>
2024-09-21 17:41:52 +0100

    ft: add in score game round component scaffold

---
+++ src/client/src/app/components/score-game-row.component.ts
@@ -0,0 +1,16 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'ws-score-game-round-row',
+  standalone: true,
+  template: `
+  <div>
+    Hello
+  </div>
+  `,
+  styles: `
+  `
+})
+export class ScoreGameRowComponent {
+
+}
